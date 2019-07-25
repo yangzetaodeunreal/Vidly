ad351612b4902854e8abee23c8b8bedaa003201d
泽韬 杨 <yangzetaodeunreal@outlook.com>
2019-07-25 16:16:24 +0800

    Revert "Revert "add movie api endpoint""
    This reverts commit e2f199aa3b2d76eeea78b783fc20444647bd5d1c.

---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime? ReleaseDate { get; set; }
+
+        public int? NumberInStock { get; set; }
+
+        public int? GenreId { get; set; }
+    }
+}
